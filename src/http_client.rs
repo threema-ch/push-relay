1b2bed593d99328382c545ecc8db92c00c807f1f
Danilo Bargen <danilo.bargen@threema.ch>
2022-02-16 14:25:38 +0100

    Upgrade hyper-rustls: 0.22 → 0.23

--- src/http_client.rs
+++ src/http_client.rs
@@ -4,15 +4,19 @@ use hyper::{
     client::{Client, HttpConnector},
     Body,
 };
-use hyper_rustls::HttpsConnector;
+use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
 
 pub type HttpClient = Client<HttpsConnector<HttpConnector>, Body>;
 
-/// Create a HTTP client instance.
+/// Create a HTTP 1 client instance.
 ///
 /// Parameter: Timeout for idle sockets being kept-alive
 pub fn make_client(pool_idle_timeout_seconds: u64) -> HttpClient {
-    let https = HttpsConnector::with_native_roots();
+    let https = HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .https_or_http()
+        .enable_http1()
+        .build();
     Client::builder()
         .pool_idle_timeout(Duration::from_secs(pool_idle_timeout_seconds))
         .build(https)
